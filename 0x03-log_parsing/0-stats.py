a3f21e6402d454594da979ef7b2517b24987757d
Vanbasten01 <fouad.yasin01@gmail.com>
2024-01-25 16:21:49 +0000

    tst

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,20 +6,21 @@ if __name__ == "__main__":
     status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
     total_size = 0
     read_lines = 0
-    status_hash = {}
+    sts_sh = {}
     try:
         for line in sys.stdin:
             args = line.split()
             if len(args) == 9:
                 status_code = int(args[-2])
                 if status_code in status_codes:
-                    status_hash[status_code] = status_hash.get(status_code, 0) + 1
+                    sts_sh[status_code] = sts_sh.get(status_code, 0) + 1
                     file_size = int(args[-1])
                     total_size += file_size
             read_lines += 1
             if read_lines % 10 == 0:
                 print(f"File size: {total_size}")
-                sorted_status_hash = {key: status_hash.get(key) for key in status_codes}
+                sorted_status_hash = {
+                        key: sts_sh.get(key) for key in status_codes}
                 for key, value in sorted_status_hash.items():
                     if value:
                         print(f"{key}: {value}")
@@ -29,4 +30,3 @@ if __name__ == "__main__":
             if value:
                 print(f"{key}: {value}")
         raise
-
