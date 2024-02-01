82725bf6809af99d6fd795cda9bb16b2f5c747c8
Vanbasten01 <fouad.yasin01@gmail.com>
2024-02-01 14:17:37 +0000

    UTF-8 Validation

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This module provides functions for text processing."""
+
+
+def validUTF8(data):
+    """Process the given text by removing special characters
+    and converting to lowercase."""
+    num_bytes = 0
+    mask = 1 << 7
+    for byte in data:
+        if not num_bytes:
+            while byte & mask:
+                num_bytes += 1
+                mask >>= 1
+            if num_bytes == 0:
+                continue
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+        num_bytes -= 1
+    return num_bytes == 0
