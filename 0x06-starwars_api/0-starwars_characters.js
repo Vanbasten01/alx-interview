0cfc21079b74431b5dd1e676bac4cc0d5a58e806
Vanbasten01 <fouad.yasin01@gmail.com>
2024-02-14 04:56:57 -0800

    tst

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,34 +2,18 @@
 
 const request = require('request');
 
-function printchars (filmId) {
-  const apiUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
-  // make a Get request
-  request(apiUrl, (error, response, body) => {
-    if (error) {
-      console.error('Error: ', error);
-    } else if (response.statusCode !== 200) {
-      console.log('status_code: ', response.statusCode);
-    } else {
-      const movieData = JSON.parse(body);
-      const characters = movieData.characters;
-      characters.forEach(characterUrl => {
-        request(characterUrl, (error, response, body) => {
-          if (error) {
-            console.error('Error: ', error);
-          } else if (response.statusCode !== 200) {
-            console.log('status_code: ', response.statusCode);
-          } else {
-            console.log(JSON.parse(body).name);
-          }
-        });
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+// make a Get request
+request(apiUrl, (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    const characters = movieData.characters;
+    characters.forEach(characterUrl => {
+      request(characterUrl, (error, response, body) => {
+        if (!error && response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
       });
-    }
-  });
-}
-
-const filmId = process.argv[2];
-
-if (filmId) {
-  printchars(filmId);
-}
+    });
+  }
+});
