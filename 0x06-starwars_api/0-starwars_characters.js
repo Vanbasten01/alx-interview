c3f7a0f43fe1a0bc96a20dd73dae8c81065e0759
Vanbasten01 <fouad.yasin01@gmail.com>
2024-02-14 05:34:36 -0800

    Star Wars Characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -8,12 +8,28 @@ request(apiUrl, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const movieData = JSON.parse(body);
     const characters = movieData.characters;
-    characters.forEach(characterUrl => {
-      request(characterUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          console.log(JSON.parse(body).name);
-        }
+
+    // Create an array to store all promises
+    const promises = characters.map(characterUrl => {
+      return new Promise((resolve, reject) => {
+        request(characterUrl, (error, response, body) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(JSON.parse(body).name);
+          }
+        });
       });
     });
+
+    // Wait for all promises to resolve
+    Promise.all(promises)
+      .then(characterNames => {
+        // Once all promises are resolved, log the character names
+        characterNames.forEach(name => console.log(name));
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
   }
 });
