b142ddee48cf913e0fff2711da4d6ce3a8172aab
Vanbasten01 <fouad.yasin01@gmail.com>
2024-03-08 16:05:00 +0100

    island perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ island parameter module"""
 
+
 def island_perimeter(grid):
     """ it calculates the island parameter """
     if len(grid) == 0:
@@ -8,9 +9,9 @@ def island_perimeter(grid):
     per = 0
     for i in range(len(grid)):
         for j in range(len(grid[0])):
-            if i > 0 and grid[i][j] == 1:
+            if grid[i][j] == 1:
                 per += 4
-                if grid[i - 1][j] == 1:
+                if i > 0 and grid[i - 1][j] == 1:
                     per -= 2
                 if j > 0 and grid[i][j - 1] == 1:
                     per -= 2
